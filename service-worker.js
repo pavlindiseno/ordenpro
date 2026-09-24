6926671466cad00aa9207bd11628ea4c09c817a4
pavlindiseno <pavlindiseno@gmail.com>
2026-09-24 12:58:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ordenpro-cache-v22';
+const CACHE_NAME = 'ordenpro-cache-v23';
 
 // Archivos propios de la app: SIEMPRE deben poder cachearse (están en el mismo sitio)
 const LOCAL_ASSETS = [
